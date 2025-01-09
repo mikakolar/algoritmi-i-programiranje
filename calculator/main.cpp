82fd24a0226a18becda7ce18c5a6c930ef718fc2
mikakolar <mika.kolar@skole.hr>
2025-01-09 14:47:03 +0100

    Add files via upload

---
+++ calculator/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+float a;
+float b;
+cout<<"Unesi 2 broja:"<<endl;
+cin>>a;
+cin>>b;
+
+cout<<a<<"+"<<b<<"="<<a+b<<end
+cout<<a<<"-"<<b<<"="<<a-b<<endl;
+cout<<a<<"*"<<b<<"="<<a*b<<endl;
+cout<<a<<"/"<<b<<"="<<a/b<<endl;
+
+}
