fe33c2a00a6abdcd0353e1f17ce6d5f1584da31f
mikakolar <mika.kolar@skole.hr>
2025-01-20 20:54:51 +0100

    Update main.cpp

--- char calc/main.cpp
+++ char calc/main.cpp
@@ -7,12 +7,12 @@ int main()
 printf("znak 1: ");
 char a;
 cin>>a;
-printf("a=%i\n",a);//pokazuje vrijednost znaka a po ASCII tablici
+printf("%c=%i\n",a,a);//pokazuje vrijednost znaka a po ASCII tablici
 
 printf("znak 2: ");
 char b;
 cin>>b;
-printf("b=%i\n",b);//pokazuje vrijednost znaka b po ASCII tablici
+printf("%c=%i\n",b,b);//pokazuje vrijednost znaka b po ASCII tablici
 
 printf("\n");//prazna linija
 
