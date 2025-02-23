5b0785b7a6d28307a91d1f4966dd29b5cd8b259a
mikakolar <mika.kolar@skole.hr>
2025-02-23 20:20:22 +0100

    Add files via upload

---
+++ scanf vjezba/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b;
+cout<<"upisi vrijednost a i b:"<<endl;
+scanf("a=%i b=%i",&a,&b);
+printf("%i + %i = %i",a,b,a+b);
+
+
+
+    return 0;
+}
